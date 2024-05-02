b43cbc4438fddf1195b85e7f5660d2dd701e961d
Niha Mukhtar Wani <niha78781@gmail.com>
2024-05-02 21:25:30 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDom from "react-dom";
 ReactDom.render(
-  <div>
-    <h1>My Favourite Subjects</h1>,
-    <ul>
-      <li>TOC</li>
-      <li>Cryptography</li>
-      <li>Java</li>
-    </ul>
-  </div>,
+     <div>
+           <h1>My Favourite Subjects</h1>,
+           <ul>
+             <li>TOC</li>
+             <li>Cryptography</li>
+             <li>Java</li>
+           </ul>
+     </div>,
   document.getElementById("root")
 );
